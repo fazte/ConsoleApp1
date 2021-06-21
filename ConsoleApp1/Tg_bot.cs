d336c9951b49a30e0fce1a4195076ccb0188b057
амир гафаров <gafarchuk@icloud.com>
2021-06-21 12:29:47 +0300

    ошибка с рутом

--- ConsoleApp1/Tg_bot.cs
+++ ConsoleApp1/Tg_bot.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
 using Telegram.Bot;
+using System.Net;
+using Newtonsoft.Json;
+using MyLib;
 
 namespace TelegramBot
 {
@@ -14,12 +17,38 @@ namespace TelegramBot
             bot.OnMessage += (s, arg) =>
             {
                 Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
-                bot.SendTextMessageAsync(arg.Message.Chat.Id, $"You say: {arg.Message.Text}");
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, $"You say: {Bot(arg.Message.Text)}");
             };
 
             bot.StartReceiving();
 
             Console.ReadKey();
+        }
+        static string Bot(string s)
+        {
+            var ApiKey = "2e0e6d59a50c44301f776b63df46a344";
+            var City = "Kazan";
+            var url = $"https://apidata.mos.ru/v1/datasets/610/rows?api_key={ApiKey}";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return "error";
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                var weatherForecast = JsonConvert.DeserializeObject<Root>(result);
+                return $"{weatherForecast}";
+            }
+           
+
         }
     }
 }
